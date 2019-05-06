8d54c11791a3bb2ed623bcf65c52ee79dd0e8374
Victor Huynh <victor.h.huynh@gmail.com>
2019-05-06 22:56:38 +0000

    added gitignore

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,26 @@
 var request = require('request');
+var secrets = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-    // ...
+    var options = {
+      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+        'User-Agent': 'request'
+        'Authorization': "c322b20d30129b4971271ffb24eb89c963c986eb"
+      }
+    };
+  
+    request(options, function(err, res, body) {
+      cb(err, body);
+    });
   }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
     console.log("Errors:", err);
     console.log("Result:", result);
   });
+
+
+  
