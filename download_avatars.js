fa967a034f7dbf7913cff45c369a1ca837f4a300
Victor Huynh <victor.h.huynh@gmail.com>
2019-05-07 01:08:18 +0000

    send help

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 var secrets = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
@@ -21,16 +22,54 @@ function getRepoContributors(repoOwner, repoName, cb) {
     });
   }
 
-  
+  var owner = process.argv[2]
+  var repo = process.argv[3]
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-    
-    for (var user of result) {
-        console.log(user.avatar_url);
+
+getRepoContributors(owner, repo, function(err, result) {
+    if(owner && repo) {
+        console.log("Errors:", err);
+        for (var user of result) {
+            var avatarUrl = user.avatar_url
+            var userLogin = './avatars/' + user.login + '.jpg'
+            downloadImageByURL(avatarUrl, userLogin)
+        } 
+    } else {
+        console.log("Please enter valid information");
     }
+
     
-    console.log("Errors:", err);
+    
   });
+
+  function downloadImageByURL(url, filePath) {
+    request.get(url) 
+    .on('error', function (err) {                                   
+             throw err; 
+               })
+    
+    .pipe(fs.createWriteStream(filePath));  
+    
+}
+
+// 
+
+// request.get('https://sytantris.github.io/http-examples/future.jpg')               
+//        .on('error', function (err) {                                   
+//          throw err; 
+//        })
+//        .on('response', function (response) {                           
+//          console.log('Response Status Code: ', response.statusCode);
+//          console.log(response.statusMessage);
+//          console.log(response.headers['content-type']);
+//        })
+//        .on('end', function() {
+//          console.log('Download Complete');
+//        })
+//        .pipe(fs.createWriteStream('./future.jpg'));  
+//        console.log('Downloading image...');
+       
+
 
 
   
